a01f40a30480e46cbed373eacee01bbe09eff19f
HemamaliniBasker <bhemamalini1999@gmail.com>
2021-03-07 18:13:54 +0530

    Handson

---
+++ Asp Handson/EmployeeAsp/EmployeeAsp/Models/ErrorViewModel.cs
@@ -0,0 +1,11 @@
+using System;
+
+namespace EmployeeAsp.Models
+{
+    public class ErrorViewModel
+    {
+        public string RequestId { get; set; }
+
+        public bool ShowRequestId => !string.IsNullOrEmpty(RequestId);
+    }
+}
